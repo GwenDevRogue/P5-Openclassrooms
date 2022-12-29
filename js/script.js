e05d209629d8a4862b5b47c6525eeaa56cfa0cda
GwendolineSegarra <gwen.segarra@gmail.com>
2022-12-29 15:29:14 +0100

    produits page index

--- js/script.js
+++ js/script.js
@@ -5,16 +5,18 @@ function main(){
 
 function displayData(data){
   console.log("j'affiche mes datas dans le html");
-  const itemsHtml = document.getElementById('items');
+  let itemsHtml = document.getElementById('items');
   data.forEach(element => {
     let productHtml = document.createElement('a');
     productHtml.href = "./product.html?id=" + element._id;
     productHtml.innerHTML = `
     <article>
-      <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+      <img src=${element.imageUrl} alt=${element.altTxt}>
       <h3 class="productName">${element.name}</h3>
-      <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>      </article>
+      <p class="productDescription">${element.description}</p>      </article>
     `
+    itemsHtml.appendChild(productHtml);
+    console.log();
   });
 }
 
